32d9b879c7deaf2cc4b972e48fbf35d6dc53c0b2
younesbardach <85373688+YounesBardach@users.noreply.github.com>
2025-08-11 20:03:41 -0400

    Change seed.js to also add a regular user.

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -4,11 +4,19 @@ import bcrypt from 'bcryptjs';
 
 async function main() {
   const adminData = {
-    name: 'jojo',
-    email: 'jojo@gmail.com',
-    username: 'jojo',
-    password: 'Jojo123!',
+    name: 'Admin User',
+    email: 'admin@example.com',
+    username: 'admin',
+    password: 'Admin123!',
     role: 'ADMIN',
+  };
+
+  const regularData = {
+    name: 'Regular User',
+    email: 'user@example.com',
+    username: 'user',
+    password: 'User123!',
+    role: 'USER',
   };
 
   const existingAdmin = await prisma.user.findFirst({
@@ -17,22 +25,41 @@ async function main() {
     },
   });
 
-  if (existingAdmin) {
-    return;
+  if (!existingAdmin) {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(adminData.password, salt);
+
+    await prisma.user.create({
+      data: {
+        name: adminData.name,
+        email: adminData.email,
+        username: adminData.username,
+        passwordHash,
+        role: adminData.role,
+      },
+    });
   }
 
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(adminData.password, salt);
-
-  await prisma.user.create({
-    data: {
-      name: adminData.name,
-      email: adminData.email,
-      username: adminData.username,
-      passwordHash,
-      role: adminData.role,
+  const existingRegular = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: regularData.email }, { username: regularData.username }],
     },
   });
+
+  if (!existingRegular) {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(regularData.password, salt);
+
+    await prisma.user.create({
+      data: {
+        name: regularData.name,
+        email: regularData.email,
+        username: regularData.username,
+        passwordHash,
+        role: regularData.role,
+      },
+    });
+  }
 }
 
 main()
